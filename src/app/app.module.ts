253eb03a52ce6c9125e14d53a679e1d184d12e28
Nestor Jr <contato.nestor@gmail.com>
2018-01-10 23:44:59 -0200

    create components, add plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PanelComponent } from './panel/panel.component';
+import { AttemptsComponent } from './attempts/attempts.component';
+import { ProgressComponent } from './progress/progress.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PanelComponent,
+    AttemptsComponent,
+    ProgressComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
