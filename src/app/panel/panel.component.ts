80846aaf9476ec77dbeafc58c296761d87108b4f
Nestor Jr <contato.nestor@gmail.com>
2018-01-12 05:31:15 -0200

    implement component: panel

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -10,10 +10,27 @@ import { PHRASES } from './phrase-mock'
 export class PanelComponent implements OnInit {
 
 	public frases: Phrase[] = PHRASES
+	public resposta: string
+	public rodada: number = 0
+	public rodadaFrase: Phrase
 
-	constructor() { console.log(this.frases) }
+	constructor() {
+		this.rodadaFrase = this.frases[this.rodada]
+		console.log(this.rodadaFrase)
+	}
 
 	ngOnInit() {
 	}
 
+	public atualizaResposta(resposta: Event): void {
+		this.resposta = ((<HTMLInputElement>resposta.target).value)
+	}
+
+	public verificarResposta(): void {
+		if(this.resposta == this.frases[0].phraseEng){
+			console.log('Resposta igual...')
+		}else{
+			console.log('Resposta diferente, gajo!')
+		}
+	}
 }
